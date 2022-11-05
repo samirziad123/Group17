c4246ee1c59e42b0e02650e53dd19b31ad38fc6a
lauren1117 <55282352+lauren1117@users.noreply.github.com>
2022-11-05 17:38:59 -0400

    removed unneccessary files

--- popup/background.js
+++
@@ -1,8 +0,0 @@
-const app = firebase.initializeApp(config);
-const auth = app.auth();
-const signInWithPopup = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  return auth.signInWithPopup(provider).catch((error) => {
-    console.log(error);
-  });
-};
--- popup/popup.js
+++
@@ -1,7 +0,0 @@
-//console.log("This is a popup!")
-// Get reference to background page.
-const bgPage = chrome.extension.getBackgroundPage();
-// Sign in with popup, typically attached to a button click.
-
-//Sign in with popup, typically attached to a button click.
-//bgPage.signInWithPopup();
