30f3a1a73eceb8869587cf07122ba04ef374dd6c
Lauren Prieto <lauren.prieto@ufl.edu>
2022-12-01 14:34:27 -0500

    less hard-coded custom

--- filters/custom.js
+++ filters/custom.js
@@ -14,7 +14,7 @@ filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0
 document.body.appendChild(filterID);
 
 
-filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="custom"> <feColorMatrix type="matrix" values="1, 1, 1, 0, 0  0, 0, 0, 0, 0  0, 0, 0, 0, 0  0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
+filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="custom"> <feColorMatrix type="matrix" values="0, 0, 0, 0, 0  0, 0, 0, 0, 0  1, 1, 0, 0, 0  0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
 stylingID.innerHTML = 'html{-webkit-filter:url(#custom);-moz-filter:(#custom);-ms-filter:(#custom);-o-filter:(#custom);filter:(#custom);}'
 setTimeout(function() {
     window.scrollBy(1, 1);
@@ -22,14 +22,19 @@ setTimeout(function() {
 }, 1);
 
 
-rScaled = 200 / 255.0
-gScaled = 100 / 255.0
-bScaled = 50 / 255.0
+rScaled = 2 / 255.0
+gScaled = 20 / 255.0
+bScaled = 100 / 255.0
+
+rBalanced = 1 - rScaled
+gBalanced = 1 - gScaled
+bBalanced = 1 - bScaled
 
 changed = document.querySelector('feColorMatrix')
 
-newFilter = rScaled + ' 0.215 0 0 0  0 ' + gScaled + ' 0.607 0 0  0 0.804 ' + bScaled + ' 0 0  0 0 0 1 0'
+newFilter = `${rScaled} ${rBalanced} 0 0 0  0 ${gScaled} ${gBalanced} 0 0  0 ${bBalanced} ${bScaled} 0 0  0 0 0 1 0`
 
+console.log(newFilter)
 
 
 changed.setAttribute(
