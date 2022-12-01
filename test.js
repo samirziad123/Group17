d52fc99f061964c424d35a57379984fe38b87216
Julie Maricic-Detweiler <juliemaricic-detweiler@Julies-MacBook-Pro-2.local>
2022-12-01 16:39:07 -0500

    Features from Sprint 1

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 tabs = document.querySelectorAll(".tabs-toggle"),
 contents = document.querySelectorAll(".tabs-content");
 
-
+//on click, activate selected tab
 tabs.forEach((tab,index) => {
    tab.addEventListener('click', () => {
       
@@ -17,6 +17,7 @@ tabs.forEach((tab,index) => {
    })
 });
 
+//on click, activate selected filter
 filters_ours = document.querySelectorAll('.filter');
 
 filters_ours.forEach((filter,index) => {
@@ -27,20 +28,44 @@ filters_ours.forEach((filter,index) => {
       })
 
       filters_ours[index].classList.add('active');
-   })
-     
+
+   })   
+    
+})
+
+custom_filters = document.querySelectorAll('.custom-filter');
+
+custom_filters.forEach((filter,index) => {
+
+   filter.addEventListener('click', () => {
+      custom_filters.forEach(filter => {
+         filter.classList.remove('active');
+      })
+
+      custom_filters[index].classList.add('active');
+
+   })   
+    
 })
 window.addEventListener('DOMContentLoaded', (event) => {
+
+   //on button click, add custom filter
    document.querySelector('.new-filter').addEventListener('click', () => {
-
-
+      
       newli = document.createElement("li")
-      newli.textContent = "Custom 2"
-      newli.classList.add('filter')
-      list = document.querySelector(".list")
+      newli.textContent = document.getElementById("filter-name").value;
+      newli.classList.add('custom-filter')
+      list = document.querySelector(".custom-list")
       list.appendChild(newli)
-
-
+      custom_filters.appendChild(newLi)
    })
+
+   //extraneous test, ignore for now
+   document.querySelector('.del-filter').addEventListener('click', () => {
+      chrome.storage.local.get([name], function(result){
+         console.log(result.value);
+      })
+
+   });
 });
 
