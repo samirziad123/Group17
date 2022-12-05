f9e33c17fe63f3d2a7f26a1a2cec8b2f6b21f867
Lauren Prieto <lauren.prieto@ufl.edu>
2022-12-05 17:33:10 -0500

    changed og filter from blue to normal

--- filters/custom.js
+++ filters/custom.js
@@ -14,7 +14,7 @@ filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0
 document.body.appendChild(filterID);
 
 
-filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="custom"> <feColorMatrix type="matrix" values="0, 0, 0, 0, 0  0, 0, 0, 0, 0  1, 1, 0, 0, 0  0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
+filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="custom"> <feColorMatrix type="matrix" values="1, 0, 0, 0, 0  0, 1, 0, 0, 0  0, 0, 1, 0, 0  0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
 stylingID.innerHTML = 'html{-webkit-filter:url(#custom);-moz-filter:(#custom);-ms-filter:(#custom);-o-filter:(#custom);filter:(#custom);}'
 setTimeout(function() {
     window.scrollBy(1, 1);
