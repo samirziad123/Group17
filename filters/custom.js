d0fc850f5bf4b522d8d8e88f294c481881d9a476
Lauren Prieto <lauren.prieto@ufl.edu>
2022-12-05 13:18:55 -0500

    improved custom functionality: filter changes based on all 3 RGB sliders

--- filters/custom.js
+++ filters/custom.js
@@ -1,7 +1,5 @@
 // custom
 
-
-
 if (document.getElementById("styleID612481")) {
     stylingID = document.getElementById("styleID612481").remove();
     filterID = document.getElementById("filterID471924").remove();
@@ -24,43 +22,37 @@ setTimeout(function() {
 }, 1);
 
 
-function applyCustom(r,g,b){
-    rScaled = r / 255.0
-    gScaled = g / 255.0
-    bScaled = b / 255.0
+function applyCustom(){
+    chrome.storage.local.get(["RGB"], function(data) {
+        
+        if(typeof data.RGB == "undefined") {
+            console.log("data.RGB is undefined")
+        }
+        else{
+            console.log("Value currently is " + data.RGB[0]);
+            
+            rScaled = data.RGB[0] / 255.0
+            gScaled = data.RGB[1] / 255.0
+            bScaled = data.RGB[2] / 255.0
+        
+            rBalanced = 1 - rScaled
+            gBalanced = 1 - gScaled
+            bBalanced = 1 - bScaled
+        
+            changed = document.querySelector('feColorMatrix')
+        
+            newFilter = `${rScaled} ${rBalanced} 0 0 0  0 ${gScaled} ${gBalanced} 0 0  0 ${bBalanced} ${bScaled} 0 0  0 0 0 1 0`
+        
+            console.log(newFilter)
+        
+        
+            changed.setAttribute(
+                'values', newFilter
+            );
+        }
+        
+    });
 
-    rBalanced = 1 - rScaled
-    gBalanced = 1 - gScaled
-    bBalanced = 1 - bScaled
-
-    changed = document.querySelector('feColorMatrix')
-
-    newFilter = `${rScaled} ${rBalanced} 0 0 0  0 ${gScaled} ${gBalanced} 0 0  0 ${bBalanced} ${bScaled} 0 0  0 0 0 1 0`
-
-    console.log(newFilter)
-
-
-    changed.setAttribute(
-        'values', newFilter
-    );
 }
 
-
-chrome.storage.local.get(["R"], function(data) {
-    
-    console.log("Value currently is " + data.R);
-    applyCustom(data.R,0,0)
-});
-
-
-// chrome.storage.local.get("R", function(data) {
-//     if(typeof R == "undefined") {
-//         console.log("undefined")
-//     } else {
-//         applyCustom(R,0,0)
-//     }
-// });
-
-
-
-
+applyCustom()
